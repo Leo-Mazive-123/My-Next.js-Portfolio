1ee9052215ac1d18101179524b6f05d97b112bab
Leo-Mazive-123 <leomazive00@gmail.com>
2025-12-05 15:50:16 +0200

    Portfolio Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
               View My Work
             </a>
             <a
-              href="/Leo T. Mazive Resume.pdf"
+              href="/Leo T. Mazive Resumee.pdf"
               download
               className="px-6 py-3 text-lg font-medium border border-blue-400 text-blue-400 hover:bg-blue-400 hover:text-white rounded-full shadow-lg transition"
             >
